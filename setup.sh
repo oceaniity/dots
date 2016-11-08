7d803db5a95fa606875cfacf9113e5303c5b66f0
oceaniity <oceaniity@gmail.com>
2016-11-08 12:26:35 +1000

    Restructure sublime folder and update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,11 +14,16 @@ ln -s $DIR/firefox/user.js ~/.mozilla/firefox/$PROFPATH/user.js
 
 echo Linking Sublime theme package
 rm -rf ~/.config/sublime-text-3/Packages/Pisco
-ln -s $DIR/sublime/Pisco ~/.config/sublime-text-3/Packages/Pisco
+ln -s $DIR/sublime/themes/pisco ~/.config/sublime-text-3/Packages/Pisco
+
+echo Installing Sublime plugins...
+rm -rf ~/.config/sublime-text-3/Packages/Chain
+ln -s $DIR/sublime/plugins/chain/ ~/.config/sublime-text-3/Packages/Chain
 
 echo Linking new Sublime preferences...
 rm ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 rm ~/.config/sublime-text-3/Packages/User/Distraction\ Free.sublime-settings
-ln -s $DIR/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-ln -s $DIR/sublime/Distraction\ Free.sublime-settings ~/.config/sublime-text-3/Packages/User/Distraction\ Free.sublime-settings
-
+rm ~/.config/sublime-text-3/Packages/User/Default\ (Linux).sublime-keymap
+ln -s $DIR/sublime/settings/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -s $DIR/sublime/settings/Distraction\ Free.sublime-settings ~/.config/sublime-text-3/Packages/User/Distraction\ Free.sublime-settings
+ln -s $DIR/sublime/settings/Default\ (Linux).sublime-keymap ~/.config/sublime-text-3/Packages/User/Default\ (Linux).sublime-keymap
